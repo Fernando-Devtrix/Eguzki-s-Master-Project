da5429cddc07bca25167bfb480f7b481e35a497b
Fernando <fernando_0970@outlook.es>
2018-06-15 16:55:20 -0500

    Add tasks feature

--- app/controllers/list.js
+++ app/controllers/list.js
@@ -1,4 +1,24 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+ tasks: [{
+    taskNumber: 1,
+    taskDescription: "Learn Ember"
+  },{
+    taskNumber: 2,
+    taskDescription: "Learn Ember2"
+  },{
+    taskNumber: 3,
+    taskDescription: "Learn Ember3"
+  }],
+  actions:{
+    addTask(newtask){
+      var tasks = this.get("tasks");
+      tasks.pushObject({
+        taskNumber: newtask,
+        taskDescription: newtask
+      });
+      this.set('newtask', '')
+    }
+  }
 });
