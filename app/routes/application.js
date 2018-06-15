9149597413a3b1630252ee97a08040923ffbfa37
Fernando <fernando_0970@outlook.es>
2018-06-14 21:02:49 -0500

    Second commit

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,17 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  // actions: {
-  //   checkUser(){
-  //     var name = this.get("name");
-  //     var pass = this.get("pass");
-  //
-  //     if (name === "fer" && pass === "pass") {
-  //       alert("Hola " + name)
-  //     //  this.transtitionTo('list');
-  //     }else {
-  //       alert("Error, no eres Eguzki");
-  //     }
-  //   }
-  // }
 });
